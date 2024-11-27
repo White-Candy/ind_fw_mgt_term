4bb1784b414912c7cd8c16430a06ec640aa5f7f2
Sugar <287276293@qq.com>
2024-11-27 08:50:30 +0800

    push

--- Assets/Scripts/Frame/UI/Panel/LoginPanel/LoginPanel.cs
+++ Assets/Scripts/Frame/UI/Panel/LoginPanel/LoginPanel.cs
@@ -13,9 +13,6 @@ public class LoginPanel : BasePanel
     private Button m_Login;
     private TMP_InputField m_Account;
     private TMP_InputField m_Pwd;
-    private Button m_ShowPwd;
-
-    private bool m_ShowPassword = true;
 
     // public static LoginPanel instance;
 
@@ -27,7 +24,6 @@ public class LoginPanel : BasePanel
         m_Login = GameObject.Find("Login").GetComponent<Button>();
         m_Account = GameObject.Find("inputAccount").GetComponent<TMP_InputField>();
         m_Pwd = GameObject.Find("inputPwd").GetComponent<TMP_InputField>();
-        m_ShowPwd = GameObject.Find("showPwd").GetComponent<Button>();
     }
 
     public void Start()
@@ -35,25 +31,6 @@ public class LoginPanel : BasePanel
         m_Login.OnClickAsObservable().Subscribe((x) => 
         {
             NetHelper.LoginReq(m_Account?.text, m_Pwd?.text);
-        });
-
-        m_ShowPwd.onClick.AddListener(() => 
-        {
-            if (m_ShowPassword)
-            {
-                m_Pwd.contentType = TMP_InputField.ContentType.Standard;
-                m_ShowPassword = false;
-            }
-            else
-            {
-                m_Pwd.contentType = TMP_InputField.ContentType.Password;
-                m_ShowPassword = true;
-            }
-
-            // refresh
-            string pwd = m_Pwd.text;
-            m_Pwd.text = "";
-            m_Pwd.text = pwd;
         });
     }
 }
