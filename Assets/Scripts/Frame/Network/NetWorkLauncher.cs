19be92745d9b0b1bcbd9327c4d01a89daec1a75b
Sugar <287276293@qq.com>
2024-11-27 13:42:19 +0800

    chore: 使用Socket组件进行网络传输。

--- Assets/Scripts/Frame/Network/NetWorkLauncher.cs
+++ Assets/Scripts/Frame/Network/NetWorkLauncher.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using UniRx;
 using UnityEngine;
@@ -15,18 +16,25 @@ public class NetWorkLauncher : MonoBehaviour
     {
         DontDestroyOnLoad(this);
 
-        await FileHelper.DownLoadTextFromServer(Application.streamingAssetsPath + "\\IP.txt", (ip) => 
+        await FileHelper.DownLoadTextFromServer(Application.streamingAssetsPath + "\\IP.txt", (url) => 
         {
-            GlobalData.IP = $"http://{ip}/";
-            // TCP.Connect(ip, 5800);
+            //GlobalData.IP = $"http://{ip}/";
+
+            string[] split = url.Split(':');
+            if (split.Count() == 2)
+            {
+                string ip = split[0];
+                string port = split[1];
+                TCP.Connect(ip, int.Parse(port));
+            }
         });
     }
 
     public void Update()
     {
-        if (HTTP.m_MessQueue.Count > 0)
+        if (TCP.m_MessQueue.Count > 0)
         {
-            MessPackage pkg = HTTP.m_MessQueue.Dequeue();
+            MessPackage pkg = TCP.m_MessQueue.Dequeue();
             //Debug.Log(pkg.ret);
             m_dispatcher.Dispatcher(pkg);
         }
